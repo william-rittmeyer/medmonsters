77114ef8d907c0cc2a8b623aebb35a05e3bcf503
William Rittmeyer <60793567+william-rittmeyer@users.noreply.github.com>
2023-05-12 13:18:49 -0400

    Login Screen

--- App.js
+++ App.js
@@ -1,48 +1,8 @@
-import React, { useEffect } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import OnboardingScreen from "./screens/OnboardingScreen";
-import LoginScreen from "./screens/LoginScreen";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-const AppStack = createStackNavigator();
+import React from 'react';
+import Providers from './navigation';
 
 const App = () => {
-    const [ isFirstLaunch, setIsFirstLaunch] = React.useState(null);
-
-    useEffect(() => {
-        AsyncStorage.getItem('alreadyLaunched').then(value => {
-            if(value ==null) {
-                AsyncStorage.setItem('alreadyLaunched', 'true');
-                setIsFirstLaunch(true);
-            } else {
-                setIsFirstLaunch(false);
-            }
-            });
-        }, []);
-
-        if( isFirstLaunch === null) {
-            return null;
-        } else if (isFirstLaunch === true) {
-            return (
-                <NavigationContainer>
-                    <AppStack.Navigator
-                        screenOptions={{
-                            headerShown: false
-                        }}
-                    >
-                        <AppStack.Screen name="Onboarding" component={OnboardingScreen}/>
-                        <AppStack.Screen name="Login" component={LoginScreen}/>
-                    </AppStack.Navigator>
-                </NavigationContainer>
-            );  
-        } else {
-            return (
-            <LoginScreen />
-            )
-        }
-
+  return <Providers />;
 }
 
 export default App;
